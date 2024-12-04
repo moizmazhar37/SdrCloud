5eea7fa88511b5eb474886fbc78ddac35da8bfa9
moizmazhar37 <moeez.ch3737@gmail.com>
2024-12-04 15:14:35 +0500

    Design Changes Resolved --Logo and Colors

--- src/config/APIConfig.js
+++ src/config/APIConfig.js
@@ -1,5 +1,5 @@
-// const url = "https://backend-283763506150.us-central1.run.app";
-const url = "http://localhost:8000";
+ const url = "https://backend-283763506150.us-central1.run.app";
+//const url = "http://localhost:8000";
 
 const auth = `${url}/auth`;
 const users = `${url}/users`;
