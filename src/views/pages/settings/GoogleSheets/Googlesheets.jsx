0377c9fca07377b2d0d70e342869f61046968143
moizmazhar37 <moeez.ch3737@gmail.com>
2024-12-19 15:57:58 +0500

    API done, Nav done, table left

---
+++ src/views/pages/settings/GoogleSheets/Googlesheets.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import FullScreenLoader from "../../../../component/FullScreenLoader";
+import styles from "./googlesheets.module.scss";
+
+import useGoogleSheetsData from "./hooks";
+
+function GoogleSheets() {
+  const { data, loading, error } = useGoogleSheetsData();
+  if (loading) {
+    return <FullScreenLoader />;
+  }
+  console.log(data);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.backNavigators}>
+        <FontAwesomeIcon icon={faArrowLeft} color="#0358AC" />
+        <div className={styles.navigationText}>
+          <p>Settings</p>/ <p>Integration</p>/{" "}
+          <p style={{ color: "#0358AC" }}>Google Sheet</p>
+        </div>
+      </div>
+      <button>Create New Gooogle Sheet Connection</button>
+    </div>
+  );
+}
+
+export default GoogleSheets;
---
+++ src/views/pages/settings/GoogleSheets/hooks.js
@@ -0,0 +1,39 @@
+// hooks.js
+import { useState, useEffect } from "react";
+import axios from "axios";
+import ApiConfig from "../../../../config/APIConfig";
+
+const useGoogleSheetsData = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios({
+        url: `${ApiConfig.googleSheet}/`,
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      if (response.status === 200) {
+        setData(response.data);
+      }
+    } catch (err) {
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, loading, error };
+};
+
+export default useGoogleSheetsData;
