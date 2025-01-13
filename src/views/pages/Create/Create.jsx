2d3c5afc7be8013e8337469f664ae33b72e4da94
moizmazhar37 <moeez.ch3737@gmail.com>
2025-01-13 11:35:16 +0500

    Initialized

---
+++ src/views/pages/Create/Create.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Card from "../settings/Card/Card";
+import styles from "./Create.module.scss";
+
+const Create = () => {
+  return <div>Create</div>;
+};
+
+export default Create;
