3c71324529be2000d8562d4cffac25d26976c771
moizmazhar37 <moeez.ch3737@gmail.com>
2025-01-14 10:20:13 +0500

    Fixes

---
+++ src/Common/NoData/NoData.jsx
@@ -0,0 +1,7 @@
+import React, { useState, useRef, useEffect } from "react";
+import image from "src/images/dataNotFound.png";
+const NoData = () => {
+  return <div> ABCD </div>;
+};
+
+export default NoData;
