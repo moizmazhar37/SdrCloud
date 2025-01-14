d3b7aa8afc00b393efe95bf0bc7d0e97ac2cf0f1
moizmazhar37 <moeez.ch3737@gmail.com>
2025-01-14 10:44:41 +0500

    No Data State FIxed

--- src/Common/NoData/NoData.jsx
+++ src/Common/NoData/NoData.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useRef, useEffect } from "react";
 import image from "src/images/dataNotFound.png";
+import styles from "./NoData.module.scss";
 const NoData = () => {
-  return <div> ABCD </div>;
+  return (
+    <div className={styles.NoDataContainer}>
+      {" "}
+      <div>
+        <img src={image} />
+      </div>{" "}
+      <p className={styles.text}> No data Available </p>
+    </div>
+  );
 };
 
 export default NoData;
