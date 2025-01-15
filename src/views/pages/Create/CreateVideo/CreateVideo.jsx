193347a8913d486129b654d0ef2db3d821785658
moizmazhar37 <moeez.ch3737@gmail.com>
2025-01-15 12:49:11 +0500

    Created Section Area

--- src/views/pages/Create/CreateVideo/CreateVideo.jsx
+++ src/views/pages/Create/CreateVideo/CreateVideo.jsx
@@ -1,28 +1,27 @@
 import CategoryForm from "src/Common/CategoryForm/CategoryForm";
+import SectionArea from "./SectionArea/SectionArea";
 import DynamicNavigator from "src/Common/DynamicNavigator/DynamicNavigator";
-import styles from './CreateVideo.module.scss';
+import styles from "./CreateVideo.module.scss";
 
 const CreateVideo = () => {
+  const navigationItems = [
+    { text: "Template", route: "/CreateTemplate" },
+    { text: "New Video Template", route: "/createtemplate&Video" },
+  ];
 
-    const navigationItems = [
-        { text: "Template", route: "/CreateTemplate" },
-        { text: "New Video Template", route: "/createtemplate&Video" },
-    ];
-    
-    return (
-        <div className={styles.wrapper}>
-            <DynamicNavigator items={navigationItems} />
-            <div className={styles.container}>
-                <div className={styles.leftComponent}>
-                    <CategoryForm />
-                </div>
-                <div className={styles.rightComponent}>
-                    {/* Left component can be a sidebar or similar */}
-                    <h1>Section form here</h1>
-                </div>
-            </div>
+  return (
+    <div className={styles.wrapper}>
+      <DynamicNavigator items={navigationItems} />
+      <div className={styles.container}>
+        <div className={styles.leftComponent}>
+          <CategoryForm />
         </div>
-    );
-}
+        <div className={styles.rightComponent}>
+          <SectionArea />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CreateVideo;
--- src/views/pages/Create/CreateVideo/SectionArea/SectionArea.jsx
+++ src/views/pages/Create/CreateVideo/SectionArea/SectionArea.jsx
@@ -1,6 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import CategoryDropdown from "../CategoryDropdown/CategoryDropdown";
 import styles from "./SectionArea.module.scss";
 
 const SectionArea = () => {
-  return <div>Section</div>;
+  const [sections, setSections] = useState([1, 2, 3, 4]);
+  const initialOptions = []; // Add your options here
+
+  const addNewSection = () => {
+    setSections([...sections, sections.length + 1]);
+  };
+
+  const handleProgressClick = () => {
+    console.log("Progress Overview clicked");
+  };
+
+  return (
+    <div className={styles.sectionAreaContainer}>
+      <div className={styles.sectionsWrapper}>
+        {sections.map((sectionNum) => (
+          <div key={sectionNum} className={styles.sectionRow}>
+            <div className={styles.sectionNumberWrapper}>
+              <div className={styles.sectionNumber}>{sectionNum}</div>
+            </div>
+            <div className={styles.sectionContent}>
+              <div className={styles.sectionLabel}>Section {sectionNum}</div>
+              <CategoryDropdown
+                options={initialOptions}
+                buttonText="Choose type"
+                onSelect={() => {}}
+                allowAddNew={false}
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <button className={styles.addSectionButton} onClick={addNewSection}>
+        + Add New Section
+      </button>
+
+      <button className={styles.progressButton} onClick={handleProgressClick}>
+        Progress Overview
+      </button>
+    </div>
+  );
 };
+
+export default SectionArea;
