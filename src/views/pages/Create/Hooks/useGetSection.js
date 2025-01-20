1a7f1ad434dc8f10419a56ccf16ff028b8f6c871
jamshaidkhalid <jamshaidkhalid32@gmail.com>
2025-01-20 11:37:18 +0500

    chore: responsive section cards

---
+++ src/views/pages/Create/Hooks/useGetSection.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import ApiConfig from "src/config/APIConfig";
+
+const useGetSections = (templateId) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getSections = async () => {
+      try {
+        setLoading(true);
+        setError(null);
+        console.log("Fetching data for template ID:", templateId);
+        const res = await axios.get(`${ApiConfig.getTemplatebyID}/${templateId}`, {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+        });
+        if (res?.status === 200) {
+          setData(res?.data);
+        }
+      } catch (error) {
+        console.error("Error fetching sheet data:", error);
+        setError(error?.response?.data?.message || "Something went wrong");
+        toast.error(error?.response?.data?.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (templateId) {
+      getSections();
+    } else {
+      setData(null);
+    }
+  }, [templateId]);
+
+  return { data, loading, error };
+};
+
+export default useGetSections;
