f92b8ddb82fe756119a73ef114be12061d217317
Moiz Mazhar <moeez.ch3737@gmail.com>
2025-02-19 15:27:08 +0500

    Dashboard Done

--- src/layouts/DashboardLayout/NavBar/LeadsDashboard/LeadsDashboard.jsx
+++ src/layouts/DashboardLayout/NavBar/LeadsDashboard/LeadsDashboard.jsx
@@ -1,28 +1,48 @@
+// LeadsDashboard.jsx
 import React from "react";
 import styles from "./LeadsDashboard.module.scss";
 import IdentifiedByMonth from "./TopCards/IdentifiedByMonth/IdentifiedByMonth";
 import MonthlySpend from "./TopCards/MonthlySpend/MonthlySpend";
 import LatestVisitors from "./TopCards/LatestVisitors/LatestVisitors";
 import PieChart from "./TopCards/PieChart/PieChart";
-import Map from "./MidSection/Map/Map";
 import VisitorsChart from "./MidSection/VisitorsChart/VisitorsChart";
+import HorizantolBarChart from "src/Common/HorizantolBarChart/HorizantolBarChart";
+
 const LeadsDashboard = () => {
   const referralData = {
     Direct: 69.2,
     Referral: 30.8,
   };
 
-  // Example of how the component can be reused with different data
-  const otherData = {
-    "Category A": 45.5,
-    "Category B": 54.5,
+  const genderData = {
+    Male: 69.2,
+    Female: 30.8,
   };
 
-  const locations = [
-    { lat: 40.7128, lng: -74.006, visits: 3 }, // New York
-    { lat: 34.0522, lng: -118.2437, visits: 3 }, // Los Angeles
-    { lat: 41.8781, lng: -87.6298, visits: 2 }, // Chicago
-    { lat: 42.3601, lng: -71.0589, visits: 2 }, // Boston
+  const incomeData = [
+    { label: "$20,000 to $44,999", value: 5 },
+    { label: "$45,000 to $59,999", value: 2 },
+    { label: "$120,000 to $149,999", value: 7 },
+    { label: "$150,000 to $199,999", value: 1 },
+    { label: "$250,000+", value: 3 },
+    { label: "Other", value: 1 },
+  ];
+
+  const ageData = [
+    { label: "25-34", value: 2 },
+    { label: "35-44", value: 1 },
+    { label: "45-54", value: 3 },
+    { label: "55-64", value: 7 },
+    { label: "65 and older", value: 6 },
+  ];
+
+  const locationData = [
+    { label: "FL", value: 4 },
+    { label: "MD", value: 3 },
+    { label: "CA", value: 3 },
+    { label: "PA", value: 2 },
+    { label: "MI", value: 1 },
+    { label: "Other", value: 11 },
   ];
 
   return (
@@ -34,8 +54,13 @@ const LeadsDashboard = () => {
         <PieChart title="Direct Vs. Referral" data={referralData} />
       </div>
       <div className={styles.midContainer}>
-        <Map locations={locations} />
         <VisitorsChart />
+      </div>
+      <div className={styles.TopContainer}>
+        <PieChart title="Gender" data={genderData} />
+        <HorizantolBarChart data={incomeData} title="Income Level" />
+        <HorizantolBarChart data={ageData} title="Age Range" />
+        <HorizantolBarChart data={locationData} title="Location" />
       </div>
     </div>
   );
