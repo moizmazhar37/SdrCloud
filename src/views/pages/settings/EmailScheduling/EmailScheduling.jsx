f1ee55d6f854bc7d39f3f86faf0217d8ed77a15d
Adnan-Khattak5 <i211141@nu.edu.pk>
2025-04-28 15:38:24 +0500

    Chore: created new component for email scheduling

---
+++ src/views/pages/settings/EmailScheduling/EmailScheduling.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Styles from './EmailSchedulng.module.scss';
+
+const EmailScheduling = () => {
+    return (
+        <div>
+            <h1>Email Scheduling</h1>
+            <p>Welcome to the Email Scheduling page!</p>
+        </div>
+    );
+};
+
+export default EmailScheduling;
