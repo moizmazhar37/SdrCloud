55bab08f4dd19382251341a1312d0e0de3a27045
Moiz Mazhar <moeez.ch3737@gmail.com>
2025-04-30 15:33:37 +0500

    CHORE: Meetings table and Prospects table shown

--- src/views/pages/DashboardAdmin/AdminDashboard/AdminDashboard.jsx
+++ src/views/pages/DashboardAdmin/AdminDashboard/AdminDashboard.jsx
@@ -15,10 +15,11 @@ import Graph from "../MainDashboard/Graph/Graph";
 import Loader from "src/Common/Loader/Loader";
 import useGetRealTimeAlerts from "../MainDashboard/Hooks/Alerts/useGetAlerts";
 import ToastManager from "src/Common/AlertToast/ToastManager/ToastManager";
+import ProspectDashboardTable from "../ProspectDashboardTable/ProspectDashboardTable";
+import MeetingsTable from "./MeetingsTable/MeetingsTable";
 
 const AdminDashboard = () => {
   const [toastMessages, setToastMessages] = useState([]);
-  // Store dateRange in a persistent state
   const [dateRange, setDateRange] = useState({
     startDate: null,
     endDate: null,
@@ -53,7 +54,6 @@ const AdminDashboard = () => {
     }
   }, [alerts, alertsLoading]);
 
-  // This function is called when the DateRangeDropdown component updates the date range
   const handleDateRangeChange = (newDateRange) => {
     console.log("Date range updated:", newDateRange);
     setDateRange(newDateRange);
@@ -83,14 +83,14 @@ const AdminDashboard = () => {
 
   const showTopTemplates = false;
 
-  // Destructure the data for easier access
+  // Destructure the data
   const {
     supportInfo,
     metrics,
     aiComponents,
     summaryStats,
     prospectsLifecycleData,
-    templates_created_data,
+    templatesCreatedData,
     templatesSentData,
     visitDurationData,
     visitorsGraphData,
@@ -99,16 +99,14 @@ const AdminDashboard = () => {
     topTemplatesData,
   } = dashboardData;
 
+  const growthRates_metrics = metrics?.growth_rates;
+  const growthRates_summaryStats = summaryStats?.growth_rates;
+
   // Table headers for users
   const tableHeaders = [
     { key: "name", label: "Name" },
-    { key: "role", label: "Role" },
-    { key: "email", label: "Email" },
-    { key: "phone", label: "Phone" },
-    { key: "active_prospects", label: "Active Prospects" },
-    // { key: "booked_meetings", label: "Booked Meetings" },
-    { key: "viewAs", label: "View As" },
-    { key: "editUser", label: "Edit User" },
+    { key: "credits", label: "Credits used" },
+    { key: "score", label: "Score" },
   ];
 
   // Table headers for templates
@@ -118,18 +116,20 @@ const AdminDashboard = () => {
     { key: "viewed_count", label: "Times Used" },
   ];
 
-  const shouldShowChange = (change) => {
-    return change !== null && change !== undefined && change !== "none";
+  const formatAmount = (value) => {
+    if (value === null) {
+      return "N/A";
+    }
+    return value;
   };
-
   return (
     <div className={styles.dashboardContainer}>
       {/* Header Section with Support Contact and Action Buttons */}
       <div className={styles.headerSection}>
         <SupportContactLabel
-          manager={supportInfo?.manager || ""}
-          phone={supportInfo?.phone || ""}
-          email={supportInfo?.email || ""}
+          manager={supportInfo.manager}
+          phone={supportInfo.phone}
+          email={supportInfo.email}
         />
         <div className={styles.actionButtons}>
           <button
@@ -141,11 +141,7 @@ const AdminDashboard = () => {
               ? "Downloading..."
               : "Download Transaction history report"}
           </button>
-          {/* Pass the current dateRange to the DateRangeDropdown component */}
-          <DateRangeDropdown
-            onDateRangeChange={handleDateRangeChange}
-            initialDateRange={dateRange}
-          />
+          <DateRangeDropdown onDateRangeChange={handleDateRangeChange} />
         </div>
       </div>
 
@@ -153,50 +149,47 @@ const AdminDashboard = () => {
       <div className={styles.metricsCardContainer}>
         <Card
           heading="Active Users"
-          amount={metrics?.activeUsers || 0}
+          amount={formatAmount(metrics.activeUsers)}
           isClickable={true}
           onClick={() => handleCardClick("Active Users")}
         />
-        <Card heading="Available Seats" amount={metrics?.availableSeats || 0} />
+        <Card
+          heading="Available Seats"
+          amount={formatAmount(metrics.availableSeats)}
+          isClickable={true}
+          onClick={() => handleCardClick("Available Seats")}
+        />
         <Card
           heading="Credits Spent"
-          amount={metrics?.creditsSpent || 0}
-          change={
-            shouldShowChange(metrics?.growth_rates?.creditsSpent)
-              ? metrics?.growth_rates?.creditsSpent
-              : undefined
-          }
+          amount={formatAmount(metrics.creditsSpent)}
+          {...(growthRates_metrics?.creditsSpent !== null && {
+            change: growthRates_metrics?.creditsSpent,
+          })}
         />
         <Card
           heading="Credits Available"
-          amount={metrics?.creditsAvailable || 0}
-          change={
-            shouldShowChange(metrics?.growth_rates?.creditsAvailable)
-              ? metrics?.growth_rates?.creditsAvailable
-              : undefined
-          }
+          amount={formatAmount(metrics.creditsAvailable)}
+          {...(growthRates_metrics?.creditsAvailable !== null && {
+            change: growthRates_metrics?.creditsAvailable,
+          })}
         />
         <Card
           heading="Sheets Connected"
-          amount={metrics?.sheetsConnected || 0}
+          amount={formatAmount(metrics.sheetsConnected)}
           isClickable={true}
           onClick={() => handleCardClick("Sheets Connected")}
-          change={
-            shouldShowChange(metrics?.growth_rates?.sheetsConnected)
-              ? metrics?.growth_rates?.sheetsConnected
-              : undefined
-          }
+          {...(growthRates_metrics?.sheetsConnected !== null && {
+            change: growthRates_metrics?.sheetsConnected,
+          })}
         />
         <Card
           heading="Templates Generated"
-          amount={metrics?.templatesGenerated || 0}
+          amount={formatAmount(metrics.templatesGenerated)}
           isClickable={true}
           onClick={() => handleCardClick("Templates Generated")}
-          change={
-            shouldShowChange(metrics?.growth_rates?.templatesGenerated)
-              ? metrics?.growth_rates?.templatesGenerated
-              : undefined
-          }
+          {...(growthRates_metrics?.templatesGenerated !== null && {
+            change: growthRates_metrics?.templatesGenerated,
+          })}
         />
       </div>
 
@@ -206,98 +199,95 @@ const AdminDashboard = () => {
         <div className={styles.prospectsCardContainer}>
           <Card
             heading="Campaigns"
-            amount={summaryStats?.campaigns || 0}
-            isClickable={true}
-            onClick={() => handleCardClick("Campaigns")}
-            change={
-              shouldShowChange(summaryStats?.growth_rates?.campaigns)
-                ? summaryStats?.growth_rates?.campaigns
-                : undefined
-            }
+            amount={formatAmount(summaryStats.campaigns)}
+            // isClickable={true}
+            // onClick={() => handleCardClick("Campaigns")}
+            // Only pass `change` if it's not null
+            {...(growthRates_summaryStats?.campaigns !== null && {
+              change: growthRates_summaryStats?.campaigns,
+            })}
           />
+
           <Card
             heading="Prospects Added"
-            amount={summaryStats?.prospectsAdded || 0}
+            amount={formatAmount(summaryStats.prospectsAdded)}
             isClickable={true}
             onClick={() => handleCardClick("Prospects Added")}
-            change={
-              shouldShowChange(summaryStats?.growth_rates?.prospectsAdded)
-                ? summaryStats?.growth_rates?.prospectsAdded
-                : undefined
-            }
+            {...(growthRates_summaryStats?.prospectsAdded !== null && {
+              change: growthRates_summaryStats?.prospectsAdded,
+            })}
           />
+
           <Card
             heading="Meetings Booked"
-            amount={summaryStats?.meetingsBooked || 0}
-            isClickable={false}
-            // onClick={() => handleCardClick("Meetings Booked")}
-            change={
-              shouldShowChange(summaryStats?.growth_rates?.meetingsBooked)
-                ? summaryStats?.growth_rates?.meetingsBooked
-                : undefined
-            }
+            amount={formatAmount(summaryStats.meetingsBooked)}
+            isClickable={true}
+            onClick={() => handleCardClick("Meetings Booked")}
+            {...(growthRates_summaryStats?.meetingsBooked !== null && {
+              change: growthRates_summaryStats?.meetingsBooked,
+            })}
           />
+
           <Card
             heading="Meeting Attended"
-            amount={summaryStats?.meetingsAttended || 0}
-            isClickable={false}
-            // onClick={() => handleCardClick("Meeting Attended")}
-            change={
-              shouldShowChange(summaryStats?.growth_rates?.meetingsAttended)
-                ? summaryStats?.growth_rates?.meetingsAttended
-                : undefined
-            }
+            amount={formatAmount(summaryStats.meetingsAttended)}
+            isClickable={true}
+            onClick={() => handleCardClick("Meeting Attended")}
+            {...(growthRates_summaryStats?.meetingsAttended !== null && {
+              change: growthRates_summaryStats?.meetingsAttended,
+            })}
           />
+
           <Card
             heading="Visitors Identified"
-            amount={summaryStats?.visitorsIdentified || 0}
+            amount={formatAmount(summaryStats.visitorsIdentified)}
             isClickable={true}
             onClick={() => handleCardClick("Visitors Identified")}
-            change={
-              shouldShowChange(summaryStats?.growth_rates?.visitorsIdentified)
-                ? summaryStats?.growth_rates?.visitorsIdentified
-                : undefined
-            }
+            {...(growthRates_summaryStats?.visitorsIdentified !== null && {
+              change: growthRates_summaryStats?.visitorsIdentified,
+            })}
           />
         </div>
       </div>
 
       {/* Analytics Overview Section */}
       <div className={styles.prospectsLifecycleContainer}>
-        {prospectsLifecycleData && (
-          <ActiveProspectsLifecycle
-            data={prospectsLifecycleData}
-            title="Active Prospects Lifecycle Over Time (Hover to Explore)"
-          />
-        )}
+        <ActiveProspectsLifecycle
+          data={prospectsLifecycleData}
+          title="Active Prospects Lifecycle Over Time (Hover to Explore)"
+        />
       </div>
 
       <div className={styles.GraphContainer}>
         <Graph
           title="Amount of Templates Created"
-          data={templates_created_data || []}
+          data={templatesCreatedData}
           type={"filters"}
         />
         <Graph
           title="Amount of HVOs/Videos Sent"
-          data={templatesSentData || []}
+          data={templatesSentData}
           type={"filters"}
         />
       </div>
 
       <div className={styles.componentSection}>
         <div className={styles.analyticsContainer}>
-          <VisitorsGraph data={visitorsGraphData || []} />
-          <UserCreditsChart creditsData={userCreditsData || []} />
+          <VisitorsGraph data={visitorsGraphData} />
+          <UserCreditsChart creditsData={userCreditsData} />
         </div>
       </div>
 
       <div className={styles.GraphContainer}>
         <Graph
           title="Visit Duration"
-          data={visitDurationData || []}
+          data={visitDurationData}
           type={"filters"}
         />
+      </div>
+
+      <div className={styles.meetingsTableContainer}>
+        <ProspectDashboardTable />
       </div>
 
       {/* Performance Overview Section */}
@@ -305,22 +295,22 @@ const AdminDashboard = () => {
         <div className={styles.topUsersContainer}>
           <TopUsers
             title="Company Users"
-            usersData={topUsersData || []}
+            usersData={topUsersData}
             tableHeaders={tableHeaders}
             showDropdown={false}
           />
-          {showTopTemplates &&
-            topTemplatesData &&
-            topTemplatesData.length > 0 && (
-              <TopUsers
-                title="Top Performing Templates"
-                usersData={topTemplatesData}
-                tableHeaders={tableHeaders2}
-                showDropdown={false}
-              />
-            )}
+          {showTopTemplates && (
+            <TopUsers
+              title="Top Performing Templates"
+              usersData={topTemplatesData}
+              tableHeaders={tableHeaders2}
+              showDropdown={false}
+            />
+          )}
         </div>
       </div>
+
+      <MeetingsTable />
 
       {/* Popup component */}
       <CardPopUpGraph
