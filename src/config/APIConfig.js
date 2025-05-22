304ed55cb9ff6147943699aca24417da35ed4d4d
afnanbashir <contactafnanb@gmail.com>
2025-05-22 12:24:48 +0500

    chore: pipelines updated

--- src/config/APIConfig.js
+++ src/config/APIConfig.js
@@ -1,6 +1,4 @@
 export const url = "https://backend-283763506150.us-central1.run.app";
-// export const url = "http://localhost:8000";
-// export const url = "http://192.168.18.98:5600";
 export const auth = `${url}/auth`;
 export const users = `${url}/users`;
 const urls = `${url}/url`;
