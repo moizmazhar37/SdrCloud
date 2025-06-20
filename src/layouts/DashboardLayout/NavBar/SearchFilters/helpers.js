7bb7b534e6bb2cbb50235415c786a358a2e63cf9
Moiz Mazhar <moeez.ch3737@gmail.com>
2025-06-20 17:35:02 +0500

    Reset Button issue fixed

--- src/layouts/DashboardLayout/NavBar/SearchFilters/helpers.js
+++ src/layouts/DashboardLayout/NavBar/SearchFilters/helpers.js
@@ -1,7 +1,7 @@
 export const transformFilters = (filters) => {
   // Format netWorth as a single string if both min and max values exist
   const formatNetWorth = (netWorth) => {
-    if (!netWorth.checked) return null;
+    if (!netWorth || !netWorth.checked) return null;
 
     const min = netWorth.minValue;
     const max = netWorth.maxValue;
@@ -18,62 +18,62 @@ export const transformFilters = (filters) => {
   };
 
   const transformed = {
-    startDate: filters.dateRange.start || null,
-    endDate: filters.dateRange.end || null,
+    startDate: filters.dateRange?.start || null,
+    endDate: filters.dateRange?.end || null,
 
-    firstName: filters.personalCriteria.name?.checked
-      ? filters.personalCriteria.name.value.first
+    firstName: filters.personalCriteria?.name?.checked
+      ? filters.personalCriteria.name.value?.first
       : null,
-    lastName: filters.personalCriteria.name?.checked
-      ? filters.personalCriteria.name.value.last
+    lastName: filters.personalCriteria?.name?.checked
+      ? filters.personalCriteria.name.value?.last
       : null,
-    email: filters.personalCriteria.email?.checked
+    email: filters.personalCriteria?.email?.checked
       ? filters.personalCriteria.email.value
       : null,
-    gender: filters.personalCriteria.gender?.checked
+    gender: filters.personalCriteria?.gender?.checked
       ? filters.personalCriteria.gender.value
       : null,
-    age: filters.personalCriteria.ageRange?.checked
+    age: filters.personalCriteria?.ageRange?.checked
       ? filters.personalCriteria.ageRange.value
       : null,
-    hasChildren: filters.personalCriteria.hasChildren?.checked
+    hasChildren: filters.personalCriteria?.hasChildren?.checked
       ? filters.personalCriteria.hasChildren.value
       : null,
-    married: filters.personalCriteria.married?.checked
+    married: filters.personalCriteria?.married?.checked
       ? filters.personalCriteria.married.value
       : null,
 
     // Location Criteria
-    state: filters.locationCriteria.state?.checked
+    state: filters.locationCriteria?.state?.checked
       ? filters.locationCriteria.state.value
       : null,
-    city: filters.locationCriteria.city?.checked
+    city: filters.locationCriteria?.city?.checked
       ? filters.locationCriteria.city.value
       : null,
-    zipCode: filters.locationCriteria.zipCode?.checked
+    zipCode: filters.locationCriteria?.zipCode?.checked
       ? filters.locationCriteria.zipCode.value
       : null,
 
     // Marketing Criteria
-    domains: filters.marketingCriteria.domains?.checked
+    domains: filters.marketingCriteria?.domains?.checked
       ? filters.marketingCriteria.domains.value
       : null,
-    referralSource: filters.marketingCriteria.referralSource?.checked
+    referralSource: filters.marketingCriteria?.referralSource?.checked
       ? filters.marketingCriteria.referralSource.value
       : null,
-    keyword: filters.marketingCriteria.keyword?.checked
+    keyword: filters.marketingCriteria?.keyword?.checked
       ? filters.marketingCriteria.keyword.value
       : null,
-    pageViewed: filters.marketingCriteria.pageViewed?.checked
+    pageViewed: filters.marketingCriteria?.pageViewed?.checked
       ? filters.marketingCriteria.pageViewed.value
       : null,
 
     // Financial Criteria
-    netWorth: formatNetWorth(filters.financialCriteria.netWorth || {}),
-    annualIncome: filters.financialCriteria.income?.checked
+    netWorth: formatNetWorth(filters.financialCriteria?.netWorth || {}),
+    annualIncome: filters.financialCriteria?.income?.checked
       ? filters.financialCriteria.income.value
       : null,
-    ownHouse: filters.financialCriteria.ownHouse?.checked
+    ownHouse: filters.financialCriteria?.ownHouse?.checked
       ? filters.financialCriteria.ownHouse.value
       : null,
   };
@@ -83,8 +83,8 @@ export const transformFilters = (filters) => {
 
 export const resetFiltersState = () => ({
   dateRange: {
-    start: null,
-    end: null,
+    start: "",
+    end: "",
   },
   personalCriteria: {
     name: {
